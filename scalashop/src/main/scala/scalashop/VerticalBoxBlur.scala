39a928f534339d6a115b6370e50079bad00f4fdb
tonyl <tony.l.aux@go-jek.com>
2019-05-23 23:11:55 +0800

    Week 1

---
+++ scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,68 @@
+package scalashop
+
+import org.scalameter._
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer (new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+    * `dst`, starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each column, `blur` traverses the pixels by going from top to
+    * bottom.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {
+      y <- 0 until dst.height
+      x <- from until end
+    } {
+      dst.update(x, y, boxBlurKernel(src, x, y, radius))
+    }
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * columns.
+    */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val cappedNumTasks = Math.min(src.width, numTasks)
+    val taskSize = src.width / cappedNumTasks
+    val tasks = (0 until src.width).grouped(taskSize).map { cols =>
+      common.task(blur(src, dst, cols.head, cols.last + 1, radius))
+    }
+
+    tasks.foreach(_.join())
+  }
+}
